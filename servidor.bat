336bb6d2aafc7752897da1a3bed63a2a4f00892c
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2026-01-14 20:57:28 -0400

    empezando api

--- servidor.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\apache\htdocs\AppSalon\public
-start /min "" php -S localhost:3000
-start "" http://localhost:3000
-start "" https://www.udemy.com/course/desarrollo-web-completo-con-html5-css3-js-php-y-mysql/learn
-exit
