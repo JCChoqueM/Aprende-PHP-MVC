32333db696e179c8369a93c52be6cc71470c4b64
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2026-01-15 22:41:03 -0400

    debugeando los controller index router

--- public/index.php
+++ public/index.php
@@ -3,10 +3,9 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\APIController;
-use Controllers\Tema1Controller;
-use Controllers\IndexController; // Solo para la ruta principal
-//use Controllers\Tema1Controller; 
-//use Controllers\Tema2Controller; 
+use Controllers\TemaController;
+use Controllers\IndexController;
+
 
 $router = new Router();
 
@@ -16,11 +15,8 @@ $router->get('/respuesta', [APIController::class, 'index']);
 $router->get('/api/tema/{tema}/ejercicio/{ejercicio}', [APIController::class, 'getEjercicio']);
 
 //$router->get('/', ['Controllers\IndexController', 'index']);
-$router->get('/tema/{tema}/ejercicio/{ejercicio}', [Tema1Controller::class, 'getEjercicio']); 
-$router->get('/tema{tema}/ejercicio{ejercicio}', [Tema1Controller::class, 'getEjercicio']); 
- $router->get('/tema1/ejercicio1', [Tema1Controller::class, 'ejercicio1']);
- $router->get('/tema1/ejercicio2', [Tema1Controller::class, 'ejercicio2']);
- $router->get('/tema1/ejercicio3', [Tema1Controller::class, 'ejercicio3']);
+$router->get('/tema/{tema}/ejercicio/{ejercicio}', [TemaController::class, 'getEjercicio']); 
+
 
 
 // Comprobar rutas
