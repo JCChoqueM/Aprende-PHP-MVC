14dd766c531dee3036d489f9604f1ccd23e93c34
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2026-01-17 02:47:32 -0400

    sandblox con tema2/ejercicio1 funcioando punto de recuperacion

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\APIController;
 use Controllers\TemaController;
+use Controllers\Tema2Controller;
 use Controllers\IndexController;
 
 
@@ -14,6 +15,7 @@ $router->get('/', [IndexController::class, 'index']);
 $router->get('/respuesta', [APIController::class, 'index']);
 $router->get('/api/tema/{tema}/ejercicio/{ejercicio}', [APIController::class, 'getEjercicio']);
 
+$router->get('/tema2/ejercicio1', [Tema2Controller::class, 'ejercicio1']);
 //$router->get('/', ['Controllers\IndexController', 'index']);
 $router->get('/tema{tema}/ejercicio{ejercicio}', [TemaController::class, 'getEjercicio']); 
 
