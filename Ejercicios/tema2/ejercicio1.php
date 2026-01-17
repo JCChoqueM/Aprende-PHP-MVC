c5c0e4b1a30be807238fdb7f0f39ab6d4ef0c35c
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2026-01-16 23:26:17 -0400

    ejercicio to Ejercicios

---
+++ Ejercicios/tema2/ejercicio1.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ejercicios\Tema1;
+
+class Ejercicio1
+{
+    public static function resolver(): array
+    {
+        return [
+            'datos' => [
+                'tema' => 'Tema 1',
+                'ejercicio' => 'Ejercicio 1',
+                'descripcion' => 'Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP.'
+            ],
+            'resultado' => 'Mi nombre es Julio Cesar Choque Mamani'
+        ];
+    }
+}
---
+++ Ejercicios/tema2/ejercicio2.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ejercicios\Tema1;
+
+class Ejercicio1
+{
+    public static function resolver(): array
+    {
+        return [
+            'datos' => [
+                'tema' => 'Tema 12',
+                'ejercicio' => 'Ejercicio 12',
+                'descripcion' => 'Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP2.'
+            ],
+            'resultado' => 'Mi nombre es Julio Cesar Choque Mamani2'
+        ];
+    }
+}
