ba694dfbc085a9c350a18104f7181b89a9368bec
JCChoqueM <jcchoquem23@gmail.com>
2026-01-23 19:08:36 -0400

    gulp js reparado?

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -4,18 +4,16 @@ import concat from 'gulp-concat';
 import { paths } from '../config/paths.js';
 
 // Archivos de la raíz → bundle.js
-export function js(done) {
-    src(paths.js, { sourcemaps: true })
+export function js() {
+    return src(paths.js, { sourcemaps: true })
         .pipe(concat('bundle.js'))
         .pipe(terser())
         .pipe(dest('./public/build/js', { sourcemaps: '.' }));
-    done();
 }
 
 // Archivos de modules/ → individuales (sin concat)
-export function jsModules(done) {
-    src(paths.jsmodules, { sourcemaps: true })
+export function jsModules() {
+    return src(paths.jsmodules, { sourcemaps: true })
         .pipe(terser())
-        .pipe(dest('./public/build/js/modules', { sourcemaps: '.' })); // ← Mantiene estructura
-    done();
+        .pipe(dest('./public/build/js/modules', { sourcemaps: '.' }));
 }
