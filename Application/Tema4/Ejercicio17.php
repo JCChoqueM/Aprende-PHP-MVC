88ed7212f46a4283fea876ca3d01bcbe6c163cd7
JCChoqueM <jcchoquem23@gmail.com>
2026-04-06 20:27:38 -0400

    Ejercicio 17

--- Application/Tema4/Ejercicio17.php
+++ Application/Tema4/Ejercicio17.php
@@ -18,8 +18,15 @@ class Ejercicio17 extends AbstractEjercicio
         // data.input.Número  → el número introducido
         // data.respuesta     → suma de los 100 números siguientes
 
-        // Tu lógica aquí
-        // $result['respuesta'] = ...
+        // Tu lógica aqui
+        $cantidad = 100; // o cualquier valor dinámico
+        $suma = 0;
+
+        for ($i = 1; $i <= $cantidad; $i++) {
+            $suma += ($numero + $i);
+        }
+
+        $result['respuesta'] = $suma;
 
         return $result;
     }
